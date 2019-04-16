e108f0d6413181b387ba7915a12e6ddefeedb927
Eric <eric.scott@acm.org>
2019-04-15 17:40:50 -0700

    Refactored namespaces to use only the slash syntax

--- src/vocabulary/core.clj
+++ src/vocabulary/core.clj
@@ -1,7 +1,7 @@
 (ns vocabulary.core
   {:doc "Defines utilities and a set of namespaces for commonly used linked data constructs, metadata of which specifies RDF namespaces, prefixes and other details."
-   :vann:preferredNamespacePrefix "voc"
-   :vann:preferredNamespaceUri
+   :vann/preferredNamespacePrefix "voc"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/ont-app/vocabulary/"
    }
   (:require
@@ -13,8 +13,8 @@
 (def ontology
   "I'm still kinda spitballing here"
   {:voc/appendix
-   {:rdf:type :rdf:Property
-    :rdfs:comment "<ns> :voc:appendix <triples>
+   {:rdf/type :rdf:Property
+    :rdfs/comment "<ns> :voc:appendix <triples>
  Asserts that <triples> describe a graph that elaborates on other attributes asserted in usual key-value metadata asserted for <ns>, e.g. asserting a dcat:mediaType relation for some dcat:downloadURL.
  "
     }})
@@ -36,7 +36,7 @@ Where
    }
   (let [nsm (meta next-ns)
         ]
-    (if-let [p (:vann:preferredNamespacePrefix nsm)]
+    (if-let [p (:vann/preferredNamespacePrefix nsm)]
       (if (set? p)
         (reduce (fn [acc v] (assoc acc v next-ns)) acc p)
         (assoc acc p next-ns))
@@ -47,7 +47,7 @@ Where
   "Returns {<prefix> <ns> ...}
 Where 
 <prefix> is declared in metadata for some <ns> with 
-  :vann:preferredNamespacePrefix 
+  :vann/preferredNamespacePrefix 
 <ns> is an instance of clojure.lang.ns available within the lexical 
   context in which the  call was made.
 "
@@ -57,7 +57,7 @@ Where
 (defn ns-to-namespace 
   "Returns <iri> for <ns>
 Where
-<iri> is an iri declared with :vann:preferredNamespaceUri in the metadata for 
+<iri> is an iri declared with :vann/preferredNamespaceUri in the metadata for 
   <ns>, or nil
 <ns> is an instance of clojure.lang.Namespace
 "
@@ -66,14 +66,14 @@ Where
               "http://xmlns.com/foaf/0.1/"))
    }
   [ns]
-  (:vann:preferredNamespaceUri (meta ns)))
+  (:vann/preferredNamespaceUri (meta ns)))
 
 (defn namespace-to-ns  []
-  "returns {<namespace> <ns> ...} for each ns with :vann:preferredNamespaceUri
+  "returns {<namespace> <ns> ...} for each ns with :vann/preferredNamespaceUri
 declaration
 "
   (let [maybe-mapping (fn [_ns]
-                        (if-let [namespace (:vann:preferredNamespaceUri
+                        (if-let [namespace (:vann/preferredNamespaceUri
                                             (meta _ns))
                                  ]
                           [namespace _ns]))
@@ -83,10 +83,14 @@ declaration
                   (map maybe-mapping
                        (all-ns))))))
 
+(defn- aliased-ns [prefix]
+  (->> prefix
+       (symbol)
+       (get (ns-aliases *ns*))))
 
-(def voc-re
-  "recognizes keyword names with properly formatted prefixes"
-  #"^([a-zA-Z]+)[:](.*)")
+(defn- prefixed-ns [prefix]
+  (get (prefix-to-ns) prefix))
+
 
 (defn iri-for 
   "Returns <iri>  for `kw` based on metadata attached to <ns>
@@ -94,42 +98,40 @@ Where
 <iri> is of the form <namespace><value>
 <kw> is a keyword of the form <prefix>:<value>
 <ns> is an instance of clojure.lang.ns
-<prefix> is declared with :vann:preferredNamespacePrefix in metadata of <ns>
+<prefix> is declared with :vann/preferredNamespacePrefix in metadata of <ns>
 <namespace> is typically of the form http://...., declared with 
-  :vann:preferredNamespaceUri in metadata of <ns>
+  :vann/preferredNamespaceUri in metadata of <ns>
 "
   {:test #(do (assert
-               (= (iri-for :foaf:homepage)
+               (= (iri-for :foaf/homepage)
                   "http://xmlns.com/foaf/0.1/homepage"))
+              (assert
+               (= (iri-for ::blah)
+                  "http://rdf.naturallexicon.org/ont-app/vocabulary/blah"))
               (assert
                (= (iri-for :http://blah)
                   "http://blah")))
    }
   [kw]
-  {:pre (keyword? kw)
+   {:pre [(keyword? kw)]
    }
   (if (#{"http:" "https:" "file:"} (namespace kw)) ;; uri scheme http://...
     (str (namespace kw) "/" (name kw))
-    ;; else we can treat first slash as a namepace marker...
-    (if-let [[_ prefix value] (re-matches voc-re (name kw))
+    (if-let [prefix (namespace kw)
              ]
-      (let [iri (-> prefix
-                    ((prefix-to-ns))
-                    ns-to-namespace)
-            ]
-        (if-not iri
+      (let [_ns (or (find-ns (symbol prefix)) ;; ::keyword
+                    (aliased-ns prefix)
+                    (prefixed-ns prefix))]
+        (if-not _ns
           (throw (Exception. (str "No URI declared for prefix '" prefix "'")))
-          (str iri
-               value)))
-      ;; else we did not match voc-re, check the aliases
-      (if-let [_ns (namespace kw)
-               ]
-        (let [iri (->> _ns
-                       (symbol)
-                       (get (ns-aliases *ns*))
-                       (ns-to-namespace))
-              ]
-          (str iri (name kw)))))))
+          (str (-> _ns
+                   (ns-to-namespace))
+               (name kw))))
+      (throw (Exception. (str "Could not find IRI for " kw))))))
+
+
+
+
 
 (defn ns-to-prefix 
   "Returns the prefix associated with <_ns>
@@ -141,13 +143,12 @@ Where
               "foaf"))
    }
   [_ns]
-  (:vann:preferredNamespacePrefix (meta _ns)))
+  (:vann/preferredNamespacePrefix (meta _ns)))
 
 (defn qname-for 
   "Returns the 'qname' URI for `kw`, or nil if ns metadata is missing. 
 Where
-<kw> is a keyword, which may either be in voc-re format, or aliased for 
-  an ns with LOD declarations in its metadata.
+  <kw> is a keyword, in a namespace with LOD declarations in its metadata.
 "
   {:test #(do
             (assert
@@ -155,35 +156,30 @@ Where
                  (= (qname-for ::blah)
                     "voc:blah")))
             (assert
-             (= (qname-for :foaf:homepage)
+             (= (qname-for :foaf/homepage)
                 "foaf:homepage")))
    }
   [kw]
   {:pre [(keyword? kw)
          ]
    }
-  (if-let [[_ prefix value] (re-matches voc-re (name kw))
+  (if-let [prefix (namespace kw)
            ]
-    (do 
-      (if (not (contains? (prefix-to-ns) prefix))
-        (throw (Exception.
-                (str "No URI declared for prefix '"
-                     prefix
-                     "' in any namespace."))))
-      (str prefix ":" value))
-    ;; else we did not match voc-re, check the aliases
-    (if-let [_ns (namespace kw)
-             ]
-      (if (#{"http:" "https:" "file:"} _ns) ;; this is a scheme, not a namespace
-        (str "<" _ns "/" (name kw) ">")
-        (str (->> (or (->> _ns
-                           (symbol)
-                           (get (ns-aliases *ns*)))
-                      ;; else there's an _ns, but no alias assume ::keyword
-                      *ns*)
-                  (ns-to-prefix))
+    (if (#{"http:" "https:" "file:"} prefix) ;; this is a scheme, not a namespace
+      (str "<" prefix "/" (name kw) ">")
+      (let [_ns (or (find-ns (symbol prefix))
+                    (aliased-ns prefix)
+                    (prefixed-ns prefix))
+            ]
+        (if-not _ns
+          (throw (Exception. (str "Could not resolve prefix " prefix))))
+        
+        (str (ns-to-prefix _ns)
              ":"
-             (name kw))))))
+             (name kw))))
+    ;; else no namespace
+    (str "<" (name kw) ">")))
+
   
 
 (defn namespace-re []
@@ -205,29 +201,27 @@ Where
 "
   {:test #(assert
            (= (keyword-for "http://xmlns.com/foaf/0.1/homepage")
-              :foaf:homepage))
+              :foaf/homepage))
    }
   [uri]
   {:pre [(string? uri)]
    }
- 
   (let [[_ namespace value] (re-matches (namespace-re) uri)
         ]
     (if (not value)
       (keyword uri)
       (if (not namespace)
         (keyword value)
-        (keyword (str (-> namespace
-                          ((namespace-to-ns))
-                          meta
-                          :vann:preferredNamespacePrefix)
-                      ":"
-                      value
-                      ))))))
+        (keyword (-> namespace
+                     ((namespace-to-ns))
+                     meta
+                     :vann/preferredNamespacePrefix)
+                 value
+                 )))))
 
 (defn- prefix-re []
   "Returns a regex that recognizes prefixes declared in ns metadata with 
-  :vann:preferredNamespacePrefix keys
+  :vann/preferredNamespacePrefix keys
 "
   (re-pattern
    (str "[^a-zA-Z]+("
@@ -238,7 +232,7 @@ Where
   "Returns #{<prefix>...} for <s>
 Where
 <prefix> is a prefix found in <s>, for which some (meta ns) has a 
-  :vann:preferredNamespacePrefix declaration
+  :vann/preferredNamespacePrefix declaration
 <s> is a string, typically a SPARQL query body for which we want to 
   infer prefix declarations.
 "
@@ -263,9 +257,9 @@ Where
 Where
 <prefix-string> := PREFIX <prefix>: <namespace>\n
 <prefix> is a prefix defined for <namespace> in metadata of some ns with 
-  :vann:preferredNamespacePrefix
+  :vann/preferredNamespacePrefix
 <namespace> is a namespace defined in the metadata for some ns with 
-  :vann:preferredNamespaceUri
+  :vann/preferredNamespaceUri
 "
   {:test #(assert
            (=
@@ -304,34 +298,34 @@ Where
 
 (ns org.naturallexicon.lod.rdf
   {
-  :dc:title "The RDF Concepts Vocabulary (RDF)" 
-  :dc:description "This is the RDF Schema for the RDF vocabulary terms
+  :dc/title "The RDF Concepts Vocabulary (RDF)" 
+  :dc/description "This is the RDF Schema for the RDF vocabulary terms
   in the RDF Namespace, defined in RDF 1.1 Concepts."
-   :vann:preferredNamespaceUri "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   :vann:preferredNamespacePrefix "rdf"
-   :foaf:homepage "https://www.w3.org/2001/sw/wiki/RDF"
-   :dcat:downloadURL "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   :voc:appendix [["http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-                   :dcat:mediaType "text/turtle"]]
+   :vann/preferredNamespaceUri "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   :vann/preferredNamespacePrefix "rdf"
+   :foaf/homepage "https://www.w3.org/2001/sw/wiki/RDF"
+   :dcat/downloadURL "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   :voc/appendix [["http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                   :dcat/mediaType "text/turtle"]]
    }
   )
 
 (ns org.naturallexicon.lod.rdf-schema
   {
-   :dc:title "The RDF Schema vocabulary (RDFS)"
-   :vann:preferredNamespaceUri "http://www.w3.org/2000/01/rdf-schema#"
-   :vann:preferredNamespacePrefix "rdfs"
-   :foaf:homepage "https://www.w3.org/TR/rdf-schema/"
-   :dcat:downloadURL "http://www.w3.org/2000/01/rdf-schema#"
-   :voc:appendix [["http://www.w3.org/2000/01/rdf-schema#"
-                   :dcat:mediaType "text/turtle"]]
+   :dc/title "The RDF Schema vocabulary (RDFS)"
+   :vann/preferredNamespaceUri "http://www.w3.org/2000/01/rdf-schema#"
+   :vann/preferredNamespacePrefix "rdfs"
+   :foaf/homepage "https://www.w3.org/TR/rdf-schema/"
+   :dcat/downloadURL "http://www.w3.org/2000/01/rdf-schema#"
+   :voc/appendix [["http://www.w3.org/2000/01/rdf-schema#"
+                   :dcat/mediaType "text/turtle"]]
    }
   )
 
 (ns org.naturallexicon.lod.owl  
 {
- :dc:title "The OWL 2 Schema vocabulary (OWL 2)"
- :dc:description
+ :dc/title "The OWL 2 Schema vocabulary (OWL 2)"
+ :dc/description
  "This ontology partially describes the built-in classes and
   properties that together form the basis of the RDF/XML syntax of OWL
   2.  The content of this ontology is based on Tables 6.1 and 6.2 in
@@ -348,115 +342,115 @@ Where
   into OWL ontologies. Importing this file into an OWL 2 DL ontology
   will cause it to become an OWL 2 Full ontology and may have other,
   unexpected, consequences."
- :vann:preferredNamespaceUri "http://www.w3.org/2002/07/owl#"
- :vann:preferredNamespacePrefix "owl"
- :foaf:homepage "https://www.w3.org/OWL/"
- :dcat:downloadURL "http://www.w3.org/2002/07/owl"
- :voc:appendix [["http://www.w3.org/2002/07/owl"
-                 :dcat:mediaType "text/turtle"]]
+ :vann/preferredNamespaceUri "http://www.w3.org/2002/07/owl#"
+ :vann/preferredNamespacePrefix "owl"
+ :foaf/homepage "https://www.w3.org/OWL/"
+ :dcat/downloadURL "http://www.w3.org/2002/07/owl"
+ :voc/appendix [["http://www.w3.org/2002/07/owl"
+                 :dcat/mediaType "text/turtle"]]
  }
 )
 
 (ns org.naturallexicon.lod.vann
 {
-  :rdfs:label "VANN"
-  :dc:description "A vocabulary for annotating vocabulary descriptions"
-  :vann:preferredNamespaceUri "http://purl.org/vocab/vann"
-  :vann:peferredNamespacePrefix "vann"
-  :foaf:homepage "http://vocab.org/vann/"
+  :rdfs/label "VANN"
+  :dc/description "A vocabulary for annotating vocabulary descriptions"
+  :vann/preferredNamespaceUri "http://purl.org/vocab/vann"
+  :vann/peferredNamespacePrefix "vann"
+  :foaf/homepage "http://vocab.org/vann/"
 })
 
 (ns org.naturallexicion.lod.dc
   {
-   :dc:title "Dublin Core Metadata Element Set, Version 1.1"
-   :vann:preferredNamespaceUri "http://purl.org/dc/elements/1.1/"
-   :vann:preferredNamespacePrefix "dc"
-   :dcat:downloadURL "http://purl.org/dc/elements/1.1/"
-   :voc:appendix [["http://purl.org/dc/elements/1.1/"
-                   :dcat:mediaType "text/turtle"]]
+   :dc/title "Dublin Core Metadata Element Set, Version 1.1"
+   :vann/preferredNamespaceUri "http://purl.org/dc/elements/1.1/"
+   :vann/preferredNamespacePrefix "dc"
+   :dcat/downloadURL "http://purl.org/dc/elements/1.1/"
+   :voc/appendix [["http://purl.org/dc/elements/1.1/"
+                   :dcat/mediaType "text/turtle"]]
    }
   )
 
 (ns org.naturallexicon.lod.dct
   {
-   :dc:title "DCMI Metadata Terms - other"
-   :vann:preferredNamespaceUri "http://purl.org/dc/elements/1.1/"
-   :vann:preferredNamespacePrefix "dct"
-   :dcat:downloadURL "http://purl.org/dc/terms/1.1/"
-   :voc:appendix [["http://purl.org/dc/elements/1.1/"
-                   :dcat:mediaType "text/turtle"]]
+   :dc/title "DCMI Metadata Terms - other"
+   :vann/preferredNamespaceUri "http://purl.org/dc/elements/1.1/"
+   :vann/preferredNamespacePrefix "dct"
+   :dcat/downloadURL "http://purl.org/dc/terms/1.1/"
+   :voc/appendix [["http://purl.org/dc/elements/1.1/"
+                   :dcat/mediaType "text/turtle"]]
    }
   )
 
 (ns org.naturallexicon.lod.shacl
   {
-   :rdfs:label "W3C Shapes Constraint Language (SHACL) Vocabulary"
-   :rdfs:comment
+   :rdfs/label "W3C Shapes Constraint Language (SHACL) Vocabulary"
+   :rdfs/comment
    "This vocabulary defines terms used in SHACL, the W3C Shapes
    Constraint Language."
-   :vann:preferredNamespaceUri "http://www.w3.org/ns/shacl#"
-   :vann:preferredNamespacePrefix "sh"
-   :foaf:homepage "https://www.w3.org/TR/shacl/"
-   :dcat:downloadURL "https://www.w3.org/ns/shacl.ttl"
+   :vann/preferredNamespaceUri "http://www.w3.org/ns/shacl#"
+   :vann/preferredNamespacePrefix "sh"
+   :foaf/homepage "https://www.w3.org/TR/shacl/"
+   :dcat/downloadURL "https://www.w3.org/ns/shacl.ttl"
    })
 
 
 (ns org.naturallexicon.lod.dcat
   {
-   :dc:title "Data Catalog vocabulary"
-   :foaf:homepage "https://www.w3.org/TR/vocab-dcat/"
-   :dcat:downloadURL "https://www.w3.org/ns/dcat.ttl"
-   :vann:preferredNamespacePrefix "dcat"
-   :vann:preferredNamespaceUri "http://www.w3.org/ns/dcat#"
+   :dc/title "Data Catalog vocabulary"
+   :foaf/homepage "https://www.w3.org/TR/vocab-dcat/"
+   :dcat/downloadURL "https://www.w3.org/ns/dcat.ttl"
+   :vann/preferredNamespacePrefix "dcat"
+   :vann/preferredNamespaceUri "http://www.w3.org/ns/dcat#"
    }
   )
 
 
 (ns org.naturallexicon.lod.foaf
 {
- :dc:title "Friend of a Friend (FOAF) vocabulary"
- :dc:description "The Friend of a Friend (FOAF) RDF vocabulary,
+ :dc/title "Friend of a Friend (FOAF) vocabulary"
+ :dc/description "The Friend of a Friend (FOAF) RDF vocabulary,
  described using W3C RDF Schema and the Web Ontology Language."
- :vann:preferredNamespaceUri "http://xmlns.com/foaf/0.1/"
- :vann:preferredNamespacePrefix "foaf"
- :foaf:homepage "http://xmlns.com/foaf/spec/"
- :dcat:downloadURL "http://xmlns.com/foaf/spec/index.rdf"
- :voc:appendix [["http://xmlns.com/foaf/spec/index.rdf"
-                 :dcat:mediaType "application/rdf+xml"]]
+ :vann/preferredNamespaceUri "http://xmlns.com/foaf/0.1/"
+ :vann/preferredNamespacePrefix "foaf"
+ :foaf/homepage "http://xmlns.com/foaf/spec/"
+ :dcat/downloadURL "http://xmlns.com/foaf/spec/index.rdf"
+ :voc/appendix [["http://xmlns.com/foaf/spec/index.rdf"
+                 :dcat/mediaType "application/rdf+xml"]]
  }
 )
 
 (ns org.naturallexicon.lod.skos
   {
-   :dc:title "SKOS Vocabulary"
-   :dc:description "An RDF vocabulary for describing the basic
+   :dc/title "SKOS Vocabulary"
+   :dc/description "An RDF vocabulary for describing the basic
    structure and content of concept schemes such as thesauri,
    classification schemes, subject heading lists, taxonomies,
    'folksonomies', other types of controlled vocabulary, and also
    concept schemes embedded in glossaries and terminologies."
-   :vann:preferredNamespaceUri "http://www.w3.org/2004/02/skos/core#"
-   :vann:preferredNamespacePrefix "skos"
-   :foaf:homepage "https://www.w3.org/2009/08/skos-reference/skos.html"
-   :dcat:downloadURL "https://www.w3.org/2009/08/skos-reference/skos.rdf"
-   :voc:appendix [["https://www.w3.org/2009/08/skos-reference/skos.rdf"
-                   :dcat:mediaType "application/rdf+xml"]]   
+   :vann/preferredNamespaceUri "http://www.w3.org/2004/02/skos/core#"
+   :vann/preferredNamespacePrefix "skos"
+   :foaf/homepage "https://www.w3.org/2009/08/skos-reference/skos.html"
+   :dcat/downloadURL "https://www.w3.org/2009/08/skos-reference/skos.rdf"
+   :voc/appendix [["https://www.w3.org/2009/08/skos-reference/skos.rdf"
+                   :dcat/mediaType "application/rdf+xml"]]   
    }
   )
 
 
 (ns org.naturallexicon.lod.schema
   {
-   :vann:preferredNamespaceUri "http://schema.org/"
-   :vann:preferredNamespacePrefix "schema"
-   :dc:description "Schema.org is a collaborative, community activity
+   :vann/preferredNamespaceUri "http://schema.org/"
+   :vann/preferredNamespacePrefix "schema"
+   :dc/description "Schema.org is a collaborative, community activity
    with a mission to create, maintain, and promote schemas for
    structured data on the Internet, on web pages, in email messages,
    and beyond. "
-   :foaf:homepage "https://schema.org/"
-   :dcat:downloadURL #{"http://schema.org/version/latest/schema.ttl"
+   :foaf/homepage "https://schema.org/"
+   :dcat/downloadURL #{"http://schema.org/version/latest/schema.ttl"
                        "http://schema.org/version/latest/schema.jsonld"}
-   :voc:appendix [["http://schema.org/version/latest/schema.ttl"
-                   :dcat:mediaType "text/turtle"]
+   :voc/appendix [["http://schema.org/version/latest/schema.ttl"
+                   :dcat/mediaType "text/turtle"]
                   ["http://schema.org/version/latest/schema.jsonld"
-                   :dcat:mediaType "application/ld+json"]]
+                   :dcat/mediaType "application/ld+json"]]
    })
--- src/vocabulary/linguistics.clj
+++ src/vocabulary/linguistics.clj
@@ -9,11 +9,11 @@
 ;; LEMON/ONTOLEX
 
 (ns org.naturallexicon.lod.ontolex
-  {:dc:title "ontolex"
-   :dc:description "A model for the representation of lexical information relative to ontologies. Core module."
-   :vann:preferredNamespacePrefix "ontolex"
-   :vann:preferredNamespaceUri "http://www.w3.org/ns/lemon/ontolex#"
-   :foaf:homepage "https://www.w3.org/2016/05/ontolex/"
+  {:dc/title "ontolex"
+   :dc/description "A model for the representation of lexical information relative to ontologies. Core module."
+   :vann/preferredNamespacePrefix "ontolex"
+   :vann/preferredNamespaceUri "http://www.w3.org/ns/lemon/ontolex#"
+   :foaf/homepage "https://www.w3.org/2016/05/ontolex/"
    :dcat:downloadURL "http://www.w3.org/ns/lemon/ontolex#"
    :voc:appendix [["http://www.w3.org/ns/lemon/ontolex#"
                    :dcat:mediaType "application/rdf+xml"]]
@@ -23,11 +23,11 @@
 
 (ns org.naturallexicon.lod.pmn
   {
-   :dc:title "Predicate Model for Ontologies (PreMOn)"
-   :dc:description "The PreMOn Ontology is an extension of lemon (W3C Ontology Lexicon Community Group, 2015) for representing predicate models and their mappings. The Core Module of the PreMOn Ontology defines the main abstractions for modelling semantic classes with their semantic roles, mappings between different predicate models, and annotations."
-   :vann:preferredNamespacePrefix "pmn"
-   :vann:preferredNamespaceUri "http://premon.fbk.eu/ontology/core#"
-   :foaf:homepage "http://premon.fbk.eu/ontology/core.html"
+   :dc/title "Predicate Model for Ontologies (PreMOn)"
+   :dc/description "The PreMOn Ontology is an extension of lemon (W3C Ontology Lexicon Community Group, 2015) for representing predicate models and their mappings. The Core Module of the PreMOn Ontology defines the main abstractions for modelling semantic classes with their semantic roles, mappings between different predicate models, and annotations."
+   :vann/preferredNamespacePrefix "pmn"
+   :vann/preferredNamespaceUri "http://premon.fbk.eu/ontology/core#"
+   :foaf/homepage "http://premon.fbk.eu/ontology/core.html"
    :owl:seeAlso "http://www.w3.org/ns/lemon/ontolex#"
    :dcat:downloadURL #{"http://premon.fbk.eu/ontology/core.ttl"
                        "http://premon.fbk.eu/ontology/core.owl"
@@ -49,11 +49,11 @@
 
 (ns org.naturallexicon.lod.nif
   {
-   :dc:title "Natural Language Interchange Format (NIF)"
-   :dc:description "The NLP Interchange Format (NIF) is an RDF/OWL-based format that aims to achieve interoperability between Natural Language Processing (NLP) tools, language resources and annotations. NIF consists of specifications, ontologies and software, which are combined under the version identifier "2.0", but are versioned individually. This ontology is developed by the NLP2RDF project (http://nlp2rdf.org) and provided as part of NIF 2.0 under CC-BY license as well as Apache 2.0. The ontology contains seven core URIs (String, RFC5147String, Context, isString, referenceContext, beginIndex, endIndex) that provide the foundation to express NLP annotations effectively in RDF."
-   :vann:preferredNamespacePrefix "nif"
-   :vann:preferredNamespaceUri "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#"
-   :foaf:homepage #{"https://github.com/NLP2RDF",
+   :dc/title "Natural Language Interchange Format (NIF)"
+   :dc/description "The NLP Interchange Format (NIF) is an RDF/OWL-based format that aims to achieve interoperability between Natural Language Processing (NLP) tools, language resources and annotations. NIF consists of specifications, ontologies and software, which are combined under the version identifier "2.0", but are versioned individually. This ontology is developed by the NLP2RDF project (http://nlp2rdf.org) and provided as part of NIF 2.0 under CC-BY license as well as Apache 2.0. The ontology contains seven core URIs (String, RFC5147String, Context, isString, referenceContext, beginIndex, endIndex) that provide the foundation to express NLP annotations effectively in RDF."
+   :vann/preferredNamespacePrefix "nif"
+   :vann/preferredNamespaceUri "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#"
+   :foaf/homepage #{"https://github.com/NLP2RDF",
                     "http://persistence.uni-leipzig.org/nlp2rdf/specification/version/version-1.0.0-rc1.html"
                     "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core/nif-core.html"
                     }
@@ -66,9 +66,9 @@
 
 (ns org.naturallexicon.rdf.ont
   {
-   :dc:description "The home of ontological constructs that apply to no particular language."
-   :vann:preferredNamespacePrefix "natlex"
-   :vann:preferredNamespaceUri
+   :dc/description "The home of ontological constructs that apply to no particular language."
+   :vann/preferredNamespacePrefix "natlex"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/ont#"
    
    })
@@ -77,18 +77,18 @@
 
 (ns org.naturallexicon.rdf.en.ont
   {
-   :dc:description "The home of ontological constructs relating to English"
-   :vann:preferredNamespacePrefix "en"
-   :vann:preferredNamespaceUri
+   :dc/description "The home of ontological constructs relating to English"
+   :vann/preferredNamespacePrefix "en"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/en/ont#"
    }
   )
 
 (ns org.naturallexicon.rdf.en.form
   {
-   :dc:description "A container for English word forms"
-   :vann:preferredNamespacePrefix "enForm"
-   :vann:preferredNamespaceUri
+   :dc/description "A container for English word forms"
+   :vann/preferredNamespacePrefix "enForm"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/en/form/"
    }
   )
@@ -97,18 +97,18 @@
 
 (ns org.naturallexicon.rdf.zh.ont
   {
-   :dc:description "The home of ontological constructs relating to Chinese"
-   :vann:preferredNamespacePrefix "zh"
-   :vann:preferredNamespaceUri
+   :dc/description "The home of ontological constructs relating to Chinese"
+   :vann/preferredNamespacePrefix "zh"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/zh/ont#"
    }
   )
 
 (ns org.naturallexicon.rdf.zh.written
   {
-   :dc:description "A container for Chinese written forms"
-   :vann:preferredNamespacePrefix "hanzi"
-   :vann:preferredNamespaceUri
+   :dc/description "A container for Chinese written forms"
+   :vann/preferredNamespacePrefix "hanzi"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/zh/written/"
    }
   )
@@ -116,9 +116,9 @@
 
 (ns org.naturallexicon.rdf.zh.cmn
   {
-   :dc:description "A container for Mandarin forms"
-   :vann:preferredNamespacePrefix "cmn"
-   :vann:preferredNamespaceUri
+   :dc/description "A container for Mandarin forms"
+   :vann/preferredNamespacePrefix "cmn"
+   :vann/preferredNamespaceUri
    "http://rdf.naturallexicon.org/zh/cmn/"
    }
   )
