f86284ccf519e66f23947fb2e1e384ef22ec8df4
Eric Scott <eric.scott@acm.org>
2020-05-21 11:06:25 -0700

    Disambiguating dependencies.

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.7.1"
-  :dependencies [
+  :dependencies [;; dep disambiguation...
+                 [com.google.errorprone/error_prone_annotations "2.1.3"]
+                 [com.google.code.findbugs/jsr305 "3.0.2"]
+                 ;; clojure
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
                  ;; 3rd party
