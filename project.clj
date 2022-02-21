2933d512270a08c6ae15fbd182a893fdac4aae0f
Eric Scott <eric.scott@acm.org>
2022-02-21 07:13:17 -0800

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ont-app/vocabulary "0.1.7-SNAPSHOT"
+(defproject ont-app/vocabulary "0.1.7"
   :description "Utilities to map between namespaced keywords and URIs, and support language-tagged strings."
   :url "https://github.com/ont-app/vocabulary"
   :license {:name "Eclipse Public License"
